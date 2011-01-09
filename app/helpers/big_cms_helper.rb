f9334042de1a828b4ef973ff4a8f44906816040a
Tony Stubblebine <tony@tonystubblebine.com>
2011-01-09 10:15:25 -0800

    Add liquid method for calling snippits

--- app/helpers/big_cms_helper.rb
+++ app/helpers/big_cms_helper.rb
@@ -3,12 +3,26 @@ module BigCmsHelper
     "foobar"
   end
 
-  def render_liquid(page)
-    opts = { "page" => page}
+  def snippit(name, *args)
+    component = @context.registers["controller"].current_cms.components.find_by_name(name)
+    opts = {"component" => component} 
+
+    args.each_with_index do |arg, i|
+      opts["param#{(i+1).to_s}"] = arg
+    end
+
+    template = Liquid::Template.parse(component.content)
+    template.render(opts, :filters => BigCms.service_configs[:liquid].filters, :registers => {"controller" => @controller}).html_safe
+  end
+
+  def render_liquid(content)
+    opts = {} 
+    opts["page"] = @page if defined?(@page)
     opts["current_site"] = current_site if defined?(current_site)
+    opts["current_layout"] = current_layout if defined?(current_layout)
     opts["current_user"] = current_user if defined?(current_user)
 
-    template = Liquid::Template.parse(page.content)
+    template = Liquid::Template.parse(content)
     template.render(opts, :filters => BigCms.service_configs[:liquid].filters, :registers => {"controller" => @controller}).html_safe
   end
 end
--- lib/big_cms/action_controller_ext.rb
+++ lib/big_cms/action_controller_ext.rb
@@ -1,8 +1,12 @@
 ActionController::Base.class_eval do
-  helper_method :current_cms
+  helper_method :current_cms, :current_layout
 
   protected
   def current_cms
     @current_cms ||= BigCms::ContentManager.first
+  end
+
+  def current_layout
+    @current_layout ||= current_cms.layouts.first || nil
   end
 end
