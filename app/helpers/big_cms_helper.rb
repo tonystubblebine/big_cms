7312be01370df48c5e8182a881264a1d2bee6e1b
Tony Stubblebine <tony@tonystubblebine.com>
2011-01-11 19:58:23 -0800

    Fix liquid template scope problems.
    - The filters seemed to want to be in a true helper file. I didn't get to the bottom of why that was.
    - There were scope problems when current_cms/layout etc. were protected.

--- app/helpers/big_cms_helper.rb
+++ app/helpers/big_cms_helper.rb
@@ -2,6 +2,33 @@ module BigCmsHelper
   def foo(a="b")
     "foobar"
   end
-  
+ 
+  def snippit(name, *args)
+    controller = @context.registers["controller"]
+    component = controller.current_cms.components.find_by_name(name)
+    opts = {"component" => component} 
+    opts["current_site"] = controller.current_site if controller.current_site
+    opts["current_layout"] = controller.current_layout if controller.current_layout
+    opts["current_user"] = controller.current_user if controller.current_user
+    opts["current_navigation"] = controller.current_cms.navigations if controller.current_cms
+
+
+    args.each_with_index do |arg, i|
+      opts["param#{(i+1).to_s}"] = arg
+    end
+
+    template = Liquid::Template.parse(component.content)
+    template.render(opts, :filters => BigCms.service_configs[:liquid].filters, :registers => {"controller" => controller}).html_safe
+  end
+
+  def navigation_selected(slug)
+    controller = @context.registers["controller"]
+    if slug_nav = controller.current_cms.all_navigations.find{|a| a.slug == slug}
+      if current_nav = controller.current_cms.all_navigations.find{|a| a.url == controller.request.path }
+         return "selected" if slug_nav.ancestors_and_self.include?(current_nav)
+      end
+    end
+    "" 
+  end 
 end
 
--- lib/big_cms/action_controller_ext.rb
+++ lib/big_cms/action_controller_ext.rb
@@ -1,7 +1,6 @@
 ActionController::Base.class_eval do
   helper_method :current_cms, :current_layout, :snippit, :navigation_selected, :render_liquid
 
-  protected
   def current_cms
     # TODO: 2011-01-11 <tony+bigcms@tonystubblebine.com> -- This is a cheap
     # default. The order by claus is there for testing. Sometimes the fixtures
@@ -13,33 +12,6 @@ ActionController::Base.class_eval do
     @current_layout ||= current_cms.layouts.first || nil
   end
 
-  def snippit(name, *args)
-    controller = @context.registers["controller"]
-    component = controller.current_cms.components.find_by_name(name)
-    opts = {"component" => component} 
-    opts["current_site"] = controller.current_site if controller.current_site
-    opts["current_layout"] = controller.current_layout if controller.current_layout
-    opts["current_user"] = controller.current_user if controller.current_user
-    opts["current_navigation"] = controller.current_cms.navigations if controller.current_cms
-
-
-    args.each_with_index do |arg, i|
-      opts["param#{(i+1).to_s}"] = arg
-    end
-
-    template = Liquid::Template.parse(component.content)
-    template.render(opts, :filters => BigCms.service_configs[:liquid].filters, :registers => {"controller" => controller}).html_safe
-  end
-
-  def navigation_selected(slug)
-    controller = @context.registers["controller"]
-    if slug_nav = controller.current_cms.all_navigations.find{|a| a.slug == slug}
-      if current_nav = controller.current_cms.all_navigations.find{|a| a.url == controller.request.path }
-         return "selected" if slug_nav.ancestors_and_self.include?(current_nav)
-      end
-    end
-    "" 
-  end
     
   def render_liquid(content)
     opts = {} 
