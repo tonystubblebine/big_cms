3076b27cfc7515c321f4f0428a8370f7fff0f4f3
Tony Stubblebine <tony@tonystubblebine.com>
2011-02-04 12:19:51 -0500

    0.0.3 - Look for current_site when making a decision about current_cms

--- lib/big_cms/action_controller_ext.rb
+++ lib/big_cms/action_controller_ext.rb
@@ -3,9 +3,9 @@ ActionController::Base.class_eval do
 
   def current_cms
     # TODO: 2011-01-11 <tony+bigcms@tonystubblebine.com> -- This is a cheap
-    # default. The order by claus is there for testing. Sometimes the fixtures
+    # default. The order by clause is there for testing. Sometimes the fixtures
     # come back in non-id order.
-    @current_cms ||= BigCms::ContentManager.find(:first, :order => "id")
+    @current_cms ||= ((defined?(current_site) and current_site.content_manager) || (BigCms::ContentManager.find(:first, :order => "id")))
   end
 
   def current_layout
